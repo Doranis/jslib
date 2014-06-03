ba6b16b1ac731c18ba19d3ce6751511e6bebbc93
Pezhvak <pezhvakimv@gmail.com>
2014-06-03 22:16:13 +0430

    geo
    fetchInfo added

--- geo/1.0/jquery.doranis.geo.js
+++ geo/1.0/jquery.doranis.geo.js
@@ -11,10 +11,20 @@
         return navigator.geolocation ? true : false;
     }
 
-    $.doranis.geo.detect = function(opts){
+    /**
+     * Locate user by getting latitude and longitude
+     * @param opts Event Functions
+     */
+    $.doranis.geo.locate = function(opts){
         opts = $.extend({process: function(data){}, decline: function(error){}}, opts);
         if(!$.doranis.geo.isSupported()) $opts.decline('geolocation is not supported by your browser, please update your browser now!');
         else
             navigator.geolocation.getCurrentPosition(opts.process, opts.decline);
+    }
+
+    $.doranis.geo.fetchInfo = function(data, success){
+        $.getJSON('http://maps.google.com/maps/api/geocode/json?latlng='+data.coords.latitude+','+data.coords.longitude+'&sensor=false', function(detail){
+            success(detail);
+        });
     }
 })(jQuery);
